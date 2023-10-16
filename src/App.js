532c8ca73e7d41061df12305fe7b37c74fd757d9
Priyanka Gupta <Priyanka.Gupta@q3infotech.net>
2023-10-16 00:54:28 -0700

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import TodoList from './component/TodoList'
 export default function App() {
   // list of all the todo items 
   const todo = ["React learning", "Props ", "List rendering", "keys", "Conditional Rendering"]
+  const listitemsn = todo.map((item, index) => <li key={index}>{item}</li>)
+  const isloggedin = true;
+  function Greet(props) {
+    if (props.isloggedin) {
+      return <h1>You logged in</h1>
+    } else {
+      return <h1>user logged out</h1>
+    }
+  }
   return (
     <div>
       <div
@@ -18,12 +27,15 @@ export default function App() {
         <h2>Todolist Application</h2>
       </div>
       {/* rendering list items using map */}
-      {
+      {/* {
           todo.map((item) =>
             <li>{item}</li>)
-        }
-      {/* <TodoForm />
-      <TodoList /> */}
+        } */}
+
+      <Greet isloggedin={true} />
+      {isloggedin?
+      <ul>{listitemsn }</ul>:<h1>you logged out</h1>
+    }
     </div>
   )
 }
