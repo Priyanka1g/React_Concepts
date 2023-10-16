052a01001c2b66b0a88ccd3eedff0adefc1e1b23
Priyanka Gupta <Priyanka.Gupta@q3infotech.net>
2023-10-15 23:21:32 -0700

    Add all the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from '@mui/material'
+import LoginForm from './component/LoginForm'
+// import Typography from "@material-ui/core/Typography"
 export default function App() {
   return (
     <div>
-      <h1>Conditional rendering, Lists and Keys</h1>
-      <Button>start</Button>
+        <h1>Conditional Rendering, List and keys</h1>
+        <LoginForm/>
+        <Button>start</Button>
     </div>
   )
 }
-
