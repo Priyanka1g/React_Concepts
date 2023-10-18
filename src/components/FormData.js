65bbf1736e8e08077d7a905ff4fec9f7b65319b3
Priyanka Gupta <Priyanka.Gupta@q3infotech.net>
2023-10-18 02:50:43 -0700

    Add a button for submiiting the data and table for showing the users data'

--- src/components/FormData.js
+++ src/components/FormData.js
@@ -1,40 +1,53 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
+import './FormData.css'
 export default function FormData() {
   // intially the object is empty for all the field 
-  const[data, setData] = useState({name:'', age:'', gender:''})
+  const [data, setData] = useState({ name: '', age: '', company: '' })
   //for storing the data of every time use renter
-  const[user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
 
-  const changeHandler = (event)=>{
-    setData({...data, [event.target.name]:event.target.value})
+  // useEffect(() => {
+  //   if (data.name && data.age && data.company) {
+  //     setUser([...user, ]);
+  //   }
+  // }, [data]);
+
+  const changeHandler = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value })
     console.log(data);
   }
-  useEffect(() => {
-    if (data.name && data.age && data.gender) {
-      setUser([...user, data]);
-    }
-  }, [data]);
-
+  const submitHandler = () => {
+    setUser([...user, data]);
+    setData({ name: '', age: '', company: '' })
+  }
   return (
-    <div>
-      <h2>Enter user data</h2>
-      <label htmlFor="name">Name:</label>
-      <input placeholder='Enter user name' value={data.name} name="name" onChange={changeHandler}></input>
-      <div>
-      <label htmlFor="name">Age:</label>
-      <input placeholder='Enter user age' value={data.age} name="age" onChange={changeHandler}></input>
+    <div className="form-container">
+      <div className="form-inputs">
+
+        <h2>Enter user data</h2>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input placeholder='Enter user name' type='text' id='name' value={data.name} name="name" onChange={changeHandler} required></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age:</label>
+          <input placeholder='Enter user age' type='number' id='age' value={data.age} name="age" onChange={changeHandler} required></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="company">Company:</label>
+          <input placeholder='Enter user Company name' id='company' type='text' value={data.company} name="company" onChange={changeHandler} required></input>
+        </div>
+        <button onClick={submitHandler}>Submit</button>
       </div>
-      <label htmlFor="name">Gender:</label>
-      <input placeholder='Enter user gender' value={data.gender} name="gender" onChange={changeHandler}></input>
-      <button>Submit</button>
-      <h2>User Data Table</h2>
+      <div className="user-table">
+        <h2>User Data Table</h2>
         <table>
           <thead>
             <tr>
               <th>Name</th>
               <th>Age</th>
-              <th>Gender</th>
+              <th>Company</th>
             </tr>
           </thead>
           <tbody>
@@ -42,11 +55,12 @@ export default function FormData() {
               <tr key={index}>
                 <td>{item.name}</td>
                 <td>{item.age}</td>
-                <td>{item.gender}</td>
+                <td>{item.company}</td>
               </tr>
             ))}
           </tbody>
         </table>
-    </div>
+      </div>
+    </div >
   )
 }
