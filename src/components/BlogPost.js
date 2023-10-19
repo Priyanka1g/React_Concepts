4812e8f3cfe47fc3b45cceea466c0fa0e2823bf3
Priyanka Gupta <Priyanka.Gupta@q3infotech.net>
2023-10-19 06:04:35 -0700

    Add some functionality

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,10 +5,7 @@ const BlogPost = ({ title, content }) => {
   return (
     <Card variant="outlined" style={{ marginBottom: '20px' }}>
       <CardContent>
-        <Typography variant="h5" component="div">
           {title}
-        </Typography>
-        <Typography variant="body2">{content}</Typography>
       </CardContent>
     </Card>
   );
